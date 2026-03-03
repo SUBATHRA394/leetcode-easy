d45e64004f475ab65682a407e563478d23ea1a45
SUBATHRA394 <144546731+SUBATHRA394@users.noreply.github.com>
2026-03-03 12:06:15 +0530

    Create longest prefix.java

---
+++ longest prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                // If index i exceeds the current string length or characters differ
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0]; // The entire first string is the common prefix
+    }
+}
